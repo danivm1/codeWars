570c26b6ced17dfa4d09919e40d17aff851825fb
daniel.mendes <daniel.mendes@newconsoftware.com.br>
2024-09-27 15:41:44 -0300

    Adicionando subset_xor_sum

--- python/subset_xor_sum.py
+++ python/subset_xor_sum.py
@@ -8,18 +8,45 @@ def subsetXORSum(nums: List[int]) -> int:
 
         print(*subSets, sep="\n")
 
+        sum = 0
+
         for subSet in subSets:
-            if len(subSet) == 0:
+            if not len(subSet):
                 continue
-            if len(subset) == 1:
+            
+            if len(subSet) == 1:
+                sum += subSet[0]
+                continue
+            
+            b = 0
+            for i in range(len(subSet)):
+                b = b ^ subSet[i]
                 
-            for i in subSet:
-                 
+            sum+=b
+        
+        print(sum)
+                    
 
-subsetXORSum([5, 1, 6])
 
-# 101 - 5
-# 001 - 1
-# 100 - 8
-# 110 - 6
-# 010 - 2
+            
+
+subsetXORSum([5, 1, 6]) # 28
+
+# None      -> 000      0
+# 5         -> 101      5
+# 1         -> 001      1
+# 6         -> 110      6
+# 5,1       -> 101
+#              001
+#              100      4
+# 5,6       -> 101
+#              110
+#              011      3
+# 1,6       -> 001
+#              110
+#              111      7
+# 5,1,6     -> 101
+#              001
+#              100
+#              110
+#              010      2
